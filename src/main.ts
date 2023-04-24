441f1714b6589f6dfe33a3284d5ed292b5683316
jkyin <yinxiaoyu888@gmail.com>
2023-04-24 09:26:51 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 import { Logger } from './logger/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+    cors: true,
+  });
   app.useLogger(app.get(Logger));
 
   await app.listen(3000).catch((error) => console.error(error));
