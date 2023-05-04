675d12c1feec64327dad5348a13a244f7e83220d
jkyin <yinxiaoyu888@gmail.com>
2023-05-04 10:43:08 +0800

    feat: 增加毫秒

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -3,6 +3,6 @@ import * as moment from 'moment-timezone';
 
 export class Logger extends ConsoleLogger {
   protected getTimestamp(): string {
-    return moment().tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss');
+    return moment().tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss.SSS');
   }
 }
