c646291f102a190a57cd9e45843191b582b20872
jkyin <yinxiaoyu888@gmail.com>
2023-05-04 09:37:31 +0800

    fix: error

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -2,15 +2,18 @@ import { ConsoleLogger } from '@nestjs/common';
 import * as dayjs from 'dayjs';
 import * as timezone from 'dayjs/plugin/timezone';
 import * as utc from 'dayjs/plugin/utc';
+import * as customParseFormat from 'dayjs/plugin/customParseFormat';
+
 import 'dayjs/locale/zh-cn';
 
 dayjs.extend(timezone);
 dayjs.extend(utc);
 dayjs.tz.setDefault('Asia/Shanghai');
+dayjs.extend(customParseFormat);
 
 export class Logger extends ConsoleLogger {
   protected getTimestamp(): string {
-    const timestamp = dayjs.tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss');
+    const timestamp = dayjs().tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss');
     return timestamp;
   }
 }
