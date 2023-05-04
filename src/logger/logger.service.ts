43fc68de465befc6dc6df8d7f3f4cc6cd716df99
jkyin <yinxiaoyu888@gmail.com>
2023-05-04 10:29:19 +0800

    moment

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,18 +1,8 @@
 import { ConsoleLogger } from '@nestjs/common';
-import * as dayjs from 'dayjs';
-import * as timezone from 'dayjs/plugin/timezone';
-import * as utc from 'dayjs/plugin/utc';
-import * as customParseFormat from 'dayjs/plugin/customParseFormat';
-
-import 'dayjs/locale/zh-cn';
-
-dayjs.extend(utc);
-dayjs.extend(timezone);
-dayjs.extend(customParseFormat);
+import * as moment from 'moment-timezone';
 
 export class Logger extends ConsoleLogger {
   protected getTimestamp(): string {
-    const timestamp = dayjs.utc().tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss');
-    return timestamp;
+    return moment().tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss');
   }
 }
