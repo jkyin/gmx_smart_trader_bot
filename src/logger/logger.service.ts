d9f6007828e383d511841fda6d05e5fa5fde7fe5
jkyin <yinxiaoyu888@gmail.com>
2023-05-15 18:48:56 +0800

    fix: time zone

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -3,6 +3,6 @@ import { dayjs } from 'src/common/day';
 
 export class Logger extends ConsoleLogger {
   protected getTimestamp(): string {
-    return dayjs().format('YYYY-MM-DD HH:mm:ss.SSS');
+    return dayjs.tz(Date.now(), 'Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss.SSS');
   }
 }
