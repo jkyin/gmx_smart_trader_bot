e0c72854e18a204048c6b038881eec89e9edf908
jkyin <yinxiaoyu888@gmail.com>
2023-05-15 19:21:26 +0800

    fix: date log

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -3,6 +3,6 @@ import { dayjs } from 'src/common/day';
 
 export class Logger extends ConsoleLogger {
   protected getTimestamp(): string {
-    return dayjs.tz(Date.now()).format('YYYY-MM-DD HH:mm:ss.SSS');
+    return dayjs().format('YYYY-MM-DD HH:mm:ss.SSS');
   }
 }
