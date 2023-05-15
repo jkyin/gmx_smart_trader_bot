31529aecaa43f8dc95e774968dff714106a01a87
jkyin <yinxiaoyu888@gmail.com>
2023-05-15 18:56:14 +0800

    test

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -3,6 +3,8 @@ import { dayjs } from 'src/common/day';
 
 export class Logger extends ConsoleLogger {
   protected getTimestamp(): string {
+    return dayjs.tz(new Date(), 'Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss.SSS');
+
     return dayjs.tz(Date.now(), 'Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss.SSS');
   }
 }
